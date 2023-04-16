4fbe6cc2f481a1d27f08d5e18086552e8e47968e
HunterKELL <95114798+HunterKELL@users.noreply.github.com>
2023-04-15 23:31:53 -0400

    Made function for finding length in meter and feet

--- index.js
+++ index.js
@@ -3,7 +3,12 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
-
+let unitNumber = 20;
+let lengthArray = []
+const feet = 3.281
+const gallon = 0.264
+const pound = 2.204
+    
 const numberInput = document.getElementById("number-input")
 const convertBtn = document.getElementById("convert-btn")
 
@@ -12,3 +17,18 @@ convertBtn.addEventListener("click", function() {
     
 })
 //console.log(numberInput)
+
+function findLength() {
+    let meterNum = unitNumber / feet
+    //console.log(meterNum)
+    let meterUnit = Number(meterNum.toFixed(3))
+    let feetNum = unitNumber * feet
+    //console.log(feetNum)
+    let feetUnit = Number(feetNum.toFixed(3))
+
+    lengthArray.push(feetUnit)
+    lengthArray.push(meterUnit) 
+}
+
+findLength()
+console.log(lengthArray)
