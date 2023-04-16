47831bdcc4aff4cb03f2c2a3de59853ad563b9f7
HunterKELL <95114798+HunterKELL@users.noreply.github.com>
2023-04-15 22:50:35 -0400

    Added id's to p elements and changed sub-titles

--- index.js
+++ index.js
@@ -3,3 +3,12 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+const numberInput = document.getElementById("number-input")
+const convertBtn = document.getElementById("convert-btn")
+
+convertBtn.addEventListener("click", function() {
+    //console.log("clicked")
+    
+})
+//console.log(numberInput)
